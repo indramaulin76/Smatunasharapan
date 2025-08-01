f96bbb109df3ee329c49f34cf7a260cfa160a3b0
Indra Maulana <indramaulin@gmail.com>
2025-08-01 22:50:37 +0700

    full update

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,35 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+// Ganti ini dengan alamat email tujuan
+$receiving_email_address = 'Smatunasharapanadm@gmail.com';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+// Ambil data dari form
+$name    = htmlspecialchars($_POST['name']);
+$email   = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+$subject = htmlspecialchars($_POST['subject']);
+$message = htmlspecialchars($_POST['message']);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+// Validasi data
+if (!$email || empty($name) || empty($subject) || empty($message)) {
+    http_response_code(400);
+    echo "Please complete the form correctly.";
+    exit;
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// Format pesan email
+$headers  = "From: $name <$email>\r\n";
+$headers .= "Reply-To: $email\r\n";
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$body  = "Name: $name\n";
+$body .= "Email: $email\n";
+$body .= "Subject: $subject\n\n";
+$body .= "Message:\n$message\n";
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+// Kirim email
+if (mail($receiving_email_address, $subject, $body, $headers)) {
+    echo "Your message has been sent. Thank you!";
+} else {
+    http_response_code(500);
+    echo "Failed to send message. Please try again later.";
+}
 ?>
